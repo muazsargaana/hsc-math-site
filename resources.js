b8afbbe90796a5e8129a562f941d6935299eb384
muazsargaana <muaz.sargaana@gmail.com>
2026-09-05 13:36:27 +1000

    Add resource index

---
+++ resources.js
@@ -0,0 +1,12 @@
+const resources = [
+  {
+    type: "folder",
+    name: "Mathematics Extension 1",
+    children: []
+  },
+  {
+    type: "folder",
+    name: "Mathematics Extension 2",
+    children: []
+  }
+];
