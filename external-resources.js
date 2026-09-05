2389976472ec73b37fae68ad3019fdc5e886174b
muazsargaana <muaz.sargaana@gmail.com>
2026-09-05 21:41:37 +1000

    Reorganise prelim and shared resources

--- external-resources.js
+++ external-resources.js
@@ -1,19 +1,72 @@
 const externalResources = [
   {
-    course: "Mathematics Extension 1",
+    course: "Mathematics Extension 1 · Preliminary",
     categories: [
       {
-        name: "Organisation Trial Papers",
+        name: "Preliminary Papers",
         children: [
           {
             type: "folder",
-            name: "Shared Drive Collection",
+            name: "2019",
             children: [
-              { type: "file", name: "2019 trial collection", url: "https://drive.google.com/drive/folders/1riB0-W_CF0UCLl7IuiTWvm5vP3VP5GTz" },
-              { type: "file", name: "2020 trial collection", url: "https://drive.google.com/drive/folders/1Z2M69S-eldc_8JDveo5yt6J1H17pjLQf" },
-              { type: "file", name: "2021 trial collection", url: "https://drive.google.com/drive/folders/1SugtjYWptNr73turEDT4uQEnpQFjbPU9" },
-              { type: "file", name: "2022 trial collection", url: "https://drive.google.com/drive/folders/1ndLsd8wJR8lAmRBcBF_yzrzztX_ZGM5z" },
-              { type: "file", name: "2023 trial collection", url: "https://drive.google.com/drive/folders/1UiOqnHiacyay3htOOwLPN_HYCI3apHB1" }
+              { type: "file", name: "Independent", url: "https://drive.google.com/drive/folders/1kApNSOBhYjlwa5P4lSN9zbyYVsTfXsCS" },
+              { type: "file", name: "PEM", url: "https://drive.google.com/drive/folders/1v9yWHGIYNwflGIU-u4DCZx7pK2qiEVYW" },
+              { type: "file", name: "S&G", url: "https://drive.google.com/drive/folders/15CNqMESXlFo-YLY7GJDQHUO1dY3cZk2Q" },
+              { type: "file", name: "Western Mathematics Exams", url: "https://drive.google.com/drive/folders/1Y9-whxOrHm5z1NxeMZwzwJNLLRD_VIxf" }
+            ]
+          },
+          {
+            type: "folder",
+            name: "2020",
+            children: [
+              { type: "file", name: "Independent", url: "https://drive.google.com/drive/folders/1jw1BYR65ShZ5UBbtJTlrZYk5STIZ457g" },
+              { type: "file", name: "PEM", url: "https://drive.google.com/drive/folders/1_5ZCIyGimIKbEeKT3-wchyhfnMChX97a" },
+              { type: "file", name: "QATs", url: "https://drive.google.com/drive/folders/1NKeV9FgPIxZ4jkk2nj53Cr49yuWd0saq" },
+              { type: "file", name: "S&G", url: "https://drive.google.com/drive/folders/1N_WzOZpiA62V096k0ssdJbjHwdfJE2vF" },
+              { type: "file", name: "Trial Maths", url: "https://drive.google.com/drive/folders/1vTIHP8PlLqjmWfSukVgPSIqhnnhiBK8b" },
+              { type: "file", name: "Western Mathematics Exams", url: "https://drive.google.com/drive/folders/1KoL70waBNnO85dNKTecmucCmFhMsHvzy" }
+            ]
+          },
+          {
+            type: "folder",
+            name: "2021",
+            children: [
+              { type: "file", name: "Independent", url: "https://drive.google.com/drive/folders/1U34NvcgU6xdmzqxyc8E0lQQJ_AV-HVVC" },
+              { type: "file", name: "NEAP", url: "https://drive.google.com/drive/folders/17nrZxr6Uxl9ihGt06VTEc0e50jNwc99T" },
+              { type: "file", name: "PEM", url: "https://drive.google.com/drive/folders/1c6o61a-2yhzrukHZfmuPQG8--c5Z_Yvk" },
+              { type: "file", name: "QATs", url: "https://drive.google.com/drive/folders/1VgbvtbJCQjrKIfA8D4r0rUXqguQ8orYY" },
+              { type: "file", name: "S&G", url: "https://drive.google.com/drive/folders/1nlF65QhIsiAQC9x3buva3EkJOZvYCFjq" },
+              { type: "file", name: "Total Education", url: "https://drive.google.com/drive/folders/1QnW4ELBMbY_aTx35eUXuN_ZQlZ7nYsT0" },
+              { type: "file", name: "Trial Maths", url: "https://drive.google.com/drive/folders/1oOOOnfNT-iDkTGe0KRtp1KtWQ4ni_d-3" },
+              { type: "file", name: "Western Mathematics Exams", url: "https://drive.google.com/drive/folders/1Iz6IHd3fbr6-6Knf6UwqTM1FwIf7x9WY" },
+              { type: "file", name: "Knox Grammar 2021", url: "https://drive.google.com/file/d/1VzSbM1EU5kAIPbh_-QhM4Tq1hUQRGAHJ/view" }
+            ]
+          },
+          {
+            type: "folder",
+            name: "2022",
+            children: [
+              { type: "file", name: "MathsBank", url: "https://drive.google.com/drive/folders/1iv0iC-LcLiOdOrvI8E_3u-oOb3LcfB9f" },
+              { type: "file", name: "PEM", url: "https://drive.google.com/drive/folders/1T3DuAq5llgy69aDKAVmh5Cfuu_sDe-E1" },
+              { type: "file", name: "QATs", url: "https://drive.google.com/drive/folders/1izxu182qERhVkBrp0CoDAjJORf1XAzi_" },
+              { type: "file", name: "S&G", url: "https://drive.google.com/drive/folders/1E4o4ow9hMAzg-64ylbDAG6bNf8Z-_M23" },
+              { type: "file", name: "Total Education", url: "https://drive.google.com/drive/folders/1iEcJMTgmqlQHYQtJJB8pLtPHx4yLgOMy" },
+              { type: "file", name: "Trial Maths", url: "https://drive.google.com/drive/folders/1pkcB5g96QQU_PC50j_R0hvMMpFUPoe-W" },
+              { type: "file", name: "Western Mathematics Exams", url: "https://drive.google.com/drive/folders/1erMXUkMhYPeAHRaIEs_I7rFstmGo0VGI" }
+            ]
+          },
+          {
+            type: "folder",
+            name: "2023",
+            children: [
+              { type: "file", name: "Al Jabr", url: "https://drive.google.com/drive/folders/1uqI8h_6MJ2QevCxOu3CL-Tq00k000xRh" },
+              { type: "file", name: "Independent", url: "https://drive.google.com/drive/folders/1ZKgKT2eGcO2RJKQEx9wfjA9ZE8hnJVRH" },
+              { type: "file", name: "NEAP", url: "https://drive.google.com/drive/folders/13LAkdMXihq_H4MHDnp-216T988eivIFU" },
+              { type: "file", name: "PEM", url: "https://drive.google.com/drive/folders/16hhMvn7riAscZHiTnEvDL4k5FazsHHhQ" },
+              { type: "file", name: "QATs", url: "https://drive.google.com/drive/folders/1nl3mcWsisa-WLYRloOkmexGFfGd4DQuO" },
+              { type: "file", name: "S&G", url: "https://drive.google.com/drive/folders/1LAtZYoBibjH3ReTqV-mN70GEdi5dYtFb" },
+              { type: "file", name: "Total Education", url: "https://drive.google.com/drive/folders/1ipeSo3aoNqP4hqzmtKiQ_wWRX3XfqtBh" },
+              { type: "file", name: "Western Mathematics Exams", url: "https://drive.google.com/drive/folders/1KxNww7FlvtQE-LJZy9rAaOEPMpVS5nE5" }
             ]
           }
         ]
@@ -23,7 +76,7 @@ const externalResources = [
         children: [
           {
             type: "folder",
-            name: "Shared Extension 1 Resources",
+            name: "Preliminary Extension 1",
             children: [
               { type: "file", name: "100 Questions", url: "https://drive.google.com/file/d/1CK6ELep6XKWJ89fMQzbuJYaFBArpyUVW/view" },
               { type: "file", name: "Binomial Theorem Past HSC Questions", url: "https://drive.google.com/file/d/1uz1l5UsPaJ78vFN__ZxlkmVwUXmj_CWa/view" },
@@ -34,11 +87,42 @@ const externalResources = [
         ]
       },
       {
+        name: "Textbooks",
+        children: [
+          {
+            type: "folder",
+            name: "Cambridge",
+            children: [
+              { type: "file", name: "Year 11 Cambridge Extension 1", url: "https://drive.google.com/file/d/1HCUHIwaVeW7xTGIJF_kzAscC452cEmNb/view" }
+            ]
+          },
+          {
+            type: "folder",
+            name: "Maths in Focus",
+            children: [
+              { type: "file", name: "Maths Extension 1 in Focus · Year 11", url: "https://drive.google.com/file/d/1z5hJGu01bgGCZa6PgruqDu52Gny7ce4Q/view" }
+            ]
+          },
+          {
+            type: "folder",
+            name: "Fitzpatrick",
+            children: [
+              { type: "file", name: "New Senior Mathematics Extension 1 · Year 11 & 12 · 2019", url: "https://drive.google.com/file/d/1T5dWQzYh55PLLxGq-y4KQ021V49WRfwt/view" }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    course: "Mathematics Extension 1",
+    categories: [
+      {
         name: "Notes",
         children: [
           {
             type: "folder",
-            name: "Shared Extension 1 Resources",
+            name: "Extension 1",
             children: [
               { type: "file", name: "Extension 1 Notes", url: "https://drive.google.com/file/d/1QjhebDdgbqnEyjQocVUyr4mKpq4lzBXo/view" }
             ]
@@ -50,67 +134,25 @@ const externalResources = [
         children: [
           {
             type: "folder",
-            name: "Year 11",
+            name: "Cambridge",
             children: [
-              {
-                type: "folder",
-                name: "Cambridge",
-                children: [
-                  { type: "file", name: "Year 11 Cambridge Extension 1", url: "https://drive.google.com/file/d/1HCUHIwaVeW7xTGIJF_kzAscC452cEmNb/view" }
-                ]
-              },
-              {
-                type: "folder",
-                name: "Maths in Focus",
-                children: [
-                  { type: "file", name: "Maths Extension 1 in Focus · Year 11", url: "https://drive.google.com/file/d/1z5hJGu01bgGCZa6PgruqDu52Gny7ce4Q/view" }
-                ]
-              },
-              {
-                type: "folder",
-                name: "Fitzpatrick",
-                children: [
-                  { type: "file", name: "New Senior Mathematics Extension 1 · Year 11 & 12 · 2019", url: "https://drive.google.com/file/d/1T5dWQzYh55PLLxGq-y4KQ021V49WRfwt/view" }
-                ]
-              }
+              { type: "file", name: "Cambridge · Mathematics Extension 1 · Year 12", url: "https://drive.google.com/file/d/1yzqK3_ATHRAhXwIK4u1tqoA1EHE4mbqD/view" },
+              { type: "file", name: "Cambridge · Extension 1 Answers", url: "https://drive.google.com/file/d/1YSdAU5SFh-8_BQ0X7ZFSjiuOvq8HLG9L/view" },
+              { type: "file", name: "Cambridge · Worked Solutions Folder", url: "https://drive.google.com/drive/folders/1KUwSCKf0NrlREnj09nv1BU1SmD2qzKRJ" }
             ]
           },
           {
             type: "folder",
-            name: "Year 12",
+            name: "Fitzpatrick",
             children: [
-              {
-                type: "folder",
-                name: "Cambridge",
-                children: [
-                  { type: "file", name: "Cambridge · Mathematics Extension 1 · Year 12", url: "https://drive.google.com/file/d/1yzqK3_ATHRAhXwIK4u1tqoA1EHE4mbqD/view" },
-                  { type: "file", name: "Cambridge · Extension 1 Answers", url: "https://drive.google.com/file/d/1YSdAU5SFh-8_BQ0X7ZFSjiuOvq8HLG9L/view" },
-                  { type: "file", name: "Cambridge · Worked Solutions Folder", url: "https://drive.google.com/drive/folders/1KUwSCKf0NrlREnj09nv1BU1SmD2qzKRJ" }
-                ]
-              },
-              {
-                type: "folder",
-                name: "Fitzpatrick",
-                children: [
-                  { type: "file", name: "New Senior Mathematics Extension 1 · Year 11 & 12 · 2019", url: "https://drive.google.com/file/d/1T5dWQzYh55PLLxGq-y4KQ021V49WRfwt/view" }
-                ]
-              },
-              {
-                type: "folder",
-                name: "Maths in Focus",
-                children: [
-                  { type: "file", name: "Maths in Focus Extension 1 · Year 12 · 2019", url: "https://drive.google.com/file/d/1e3ib8kst98WUF0bjfBlaTVnniq5Gmn0Y/view" }
-                ]
-              }
+              { type: "file", name: "New Senior Mathematics Extension 1 · Year 11 & 12 · 2019", url: "https://drive.google.com/file/d/1T5dWQzYh55PLLxGq-y4KQ021V49WRfwt/view" }
             ]
           },
           {
             type: "folder",
-            name: "External Libraries",
+            name: "Maths in Focus",
             children: [
-              { type: "file", name: "HSC Year 11 Textbook Library", url: "https://drive.google.com/drive/folders/1-RKIkBrxDjzOibY6dR6OBHv8kq4_pM63" },
-              { type: "file", name: "HSC Year 12 Textbook Library", url: "https://drive.google.com/drive/folders/1uvKl6Ju-8PaBPKsHaTBZgaPrZOmOfKyg" },
-              { type: "file", name: "SchoolTextbooks · MediaFire", url: "https://www.mediafire.com/folder/0zir01vwtaxh9/SchoolTextbooks#0zir01vwtaxh9" }
+              { type: "file", name: "Maths in Focus Extension 1 · Year 12 · 2019", url: "https://drive.google.com/file/d/1e3ib8kst98WUF0bjfBlaTVnniq5Gmn0Y/view" }
             ]
           }
         ]
@@ -172,12 +214,23 @@ const externalResources = [
             children: [
               { type: "file", name: "HSC Mathematics Extension 2 Textbook · Steven Howard", url: "https://drive.google.com/file/d/1nqjMOC_eR6fggshfgBd869U5HvgatlhE/view" }
             ]
-          },
+          }
+        ]
+      }
+    ]
+  },
+  {
+    course: "Shared Resources",
+    categories: [
+      {
+        name: "Textbook Libraries",
+        children: [
           {
             type: "folder",
-            name: "External Libraries",
+            name: "Libraries",
             children: [
-              { type: "file", name: "HSC Year 12 Textbook Library", url: "https://drive.google.com/drive/folders/1uvKl6Ju-8PaBPKsHaTBZgaPrZOmOfKyg" },
+              { type: "file", name: "Year 11 Textbook Library", url: "https://drive.google.com/drive/folders/1-RKIkBrxDjzOibY6dR6OBHv8kq4_pM63" },
+              { type: "file", name: "Year 12 Textbook Library", url: "https://drive.google.com/drive/folders/1uvKl6Ju-8PaBPKsHaTBZgaPrZOmOfKyg" },
               { type: "file", name: "SchoolTextbooks · MediaFire", url: "https://www.mediafire.com/folder/0zir01vwtaxh9/SchoolTextbooks#0zir01vwtaxh9" }
             ]
           }
